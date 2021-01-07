bdf1dbb7582c0be13d90cfe95acc0ab0ba98c1a1
Zulfat <admin@telegrach.ru>
2021-01-07 10:10:37 +0300

    begin

---
+++ script.js
@@ -0,0 +1,27 @@
+let money;
+let time;
+
+money = prompt("Ваш бюджет на месяц?");
+time = prompt('Введите дату в формате YYYY-MM-DD');
+
+let appData = {
+   budget: money,
+   timeData: time,
+   expenses: {},
+   optionalExpenses: {},
+   income: [],
+   savings: false,
+};
+
+let q1 = prompt('Введите обязательную статью расходов в этом месяце'),
+   q2 = prompt('Во сколько обойдется?'),
+   q3 = prompt('Введите обязательную статью расходов в этом месяце'),
+   q4 = prompt('Во сколько обойдется?');
+
+
+appData.expenses.q1 = q2;
+appData.expenses.q2 = q3;
+
+appData.budget = appData.budget / 30;
+
+alert(appData.budget);
