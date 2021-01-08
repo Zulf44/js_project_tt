3a138ed402266ea73ebce0d446a298fd7ac1c099
Zulfat <admin@telegrach.ru>
2021-01-08 13:28:18 +0300

    add checking

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let money;
-let time;
+'use strict';
 
-money = prompt("Ваш бюджет на месяц?");
-time = prompt('Введите дату в формате YYYY-MM-DD');
+
+let money = prompt("Ваш бюджет на месяц?", ""),
+   time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
    budget: money,
@@ -10,18 +10,42 @@ let appData = {
    expenses: {},
    optionalExpenses: {},
    income: [],
-   savings: false,
+   savings: false
 };
 
-let q1 = prompt('Введите обязательную статью расходов в этом месяце'),
-   q2 = prompt('Во сколько обойдется?'),
-   q3 = prompt('Введите обязательную статью расходов в этом месяце'),
-   q4 = prompt('Во сколько обойдется?');
+
+for (let i = 0; i < 2; i++) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = prompt("Во сколько обойдется?", "");
+
+   if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+
+      console.log("done");
+
+      appData.expenses[a] = b;
+   } else {
+      console.log("bad result");
+      i--;
+   }
+
+};
 
 
-appData.expenses.q1 = q2;
-appData.expenses.q2 = q3;
 
-appData.budget = appData.budget / 30;
 
-alert(appData.budget);
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+   console.log("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+   console.log("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+   console.log("Это высокий уровень достатка!");
+} else {
+   console.log("Произошла ошибка");
+}
