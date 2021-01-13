2ca67d98e5884f499d3ce3c30e017e89e0761124
Zulfat <admin@telegrach.ru>
2021-01-13 22:13:39 +0300

    название коммита

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 'use strict';
 
 
-let money = prompt("Ваш бюджет на месяц?", ""),
-   time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let money,
+   time;
+
+function start() {
+   money = +prompt("Ваш бюджет на месяц?", ""),
+      time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+   while (isNaN(money) || money == "" || money == null) {
+      money = +prompt("Ваш бюджет на месяц?", "")
+   }
+
+}
+
+start();
 
 let appData = {
    budget: money,
@@ -18,7 +30,7 @@ for (let i = 0; i < 2; i++) {
    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
       b = prompt("Во сколько обойдется?", "");
 
-   if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+   if (typeof (a) === "string" && typeof (b) != null && typeof (a) != null && a != "" && b != "" && a.length < 50) {
 
       console.log("done");
 
@@ -31,8 +43,23 @@ for (let i = 0; i < 2; i++) {
 };
 
 
+let i == 0
 
+while (i < 2) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = prompt("Во сколько обойдется?", "");
+   if (typeof (a) === "string" && typeof (b) != null && typeof (a) != null && a != "" && b != "" && a.length < 50) {
 
+      console.log("done");
+
+      appData.expenses[a] = b;
+
+      i++;
+   } else {
+      console.log("bad result");
+
+   }
+}
 
 appData.moneyPerDay = appData.budget / 30;
 
